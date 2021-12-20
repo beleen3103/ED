fafa4e4ce00e8ad2d2b9edd5518167eb9c6ad9eb
beleen3103 <56888288+beleen3103@users.noreply.github.com>
2021-12-20 10:24:05 +0100

    Add files via upload

---
+++ C2-4__AlumnoAfortunado/main.cpp
@@ -0,0 +1,71 @@
+﻿// Nombre del alumno .....
+// Usuario del Juez ......
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "Queue.h"
+using namespace std;
+
+
+// función que resuelve el problema
+int resolver(Queue<int>& clase, int const salto) {
+	int fuera = salto;
+	while (clase.size() > 1) {
+		if (fuera == 0) {
+			clase.pop_front();
+			fuera = salto;
+		}
+		else {
+			clase.push_back(clase.front());
+			clase.pop_front();
+			fuera--;
+		}
+		
+	}
+	return clase.front();
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int N, salto;
+	cin >> N >> salto;
+	if (N == 0 && salto == 0)
+		return false;
+	Queue<int> clase;
+	for (int i = 0; i < N; i++) {
+		clase.push_back(i+1);
+	}
+	int afortunado = resolver(clase, salto);
+
+	// escribir sol
+
+	cout << afortunado << "\n";
+	return true;
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	while (resuelveCaso())
+		;
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
---
+++ C2-5__ReordenarCola/main.cpp
@@ -0,0 +1,61 @@
+﻿// Nombre del alumno .....
+// Usuario del Juez ......
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <deque>
+#include "Queue.h"
+using namespace std;
+
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	deque<int> lista;
+	int n, aux;
+	cin >> n;
+	if (n == 0)
+		return false;
+	for (int i = 0; i < n; i++) {
+		cin >> aux;
+		if (aux > 0) lista.push_back(aux);
+		else lista.push_front(aux);
+	}
+
+	Queue<int> sol;
+	while (!lista.empty()) {
+		sol.push_back(lista.front());
+		lista.pop_front();
+	}
+
+
+	// escribir sol
+	cout << sol << "\n";
+	return true;
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	while (resuelveCaso())
+		;
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
