3c64973594e05fe57ce6d4d8a1f32aba2aaef91f
beleen3103 <56888288+beleen3103@users.noreply.github.com>
2021-12-20 10:13:44 +0100

    Add files via upload

---
+++ C1-2__NumerosFelices/main.cpp
@@ -0,0 +1,73 @@
+﻿// Nombre del alumno .....
+// Usuario del Juez ......
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "SetOfInts1.h"
+using namespace std;
+
+
+// función que resuelve el problema
+void resolver(int n, SetOfInts1& sol, bool& feliz) {
+	int a = 0; bool s = false;
+	while (n >= 10) {
+		int s = n % 10;
+		a += (n % 10)*(n % 10);
+		n = n / 10;
+	}
+	a += n * n;
+
+	s = sol.contains(a);
+	sol.add(a);
+	if (a == 1) {
+		feliz = true;
+	}
+	else if (!s) resolver(a, sol, feliz);
+}
+
+
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int n;
+	cin >> n;
+	if (!std::cin)
+		return false;
+	SetOfInts1 sol;
+	sol.add(n);
+	bool feliz = false;
+	if (n == 1) feliz = true;
+	else resolver(n, sol, feliz);
+
+	// escribir sol
+	cout << sol;
+	cout << (feliz ? " 1" : " 0") << "\n";
+	return true;
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	while (resuelveCaso())
+		;
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
---
+++ C1-3__kElementosMayores/main.cpp
@@ -0,0 +1,96 @@
+﻿
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "Set.h"
+#include <string>
+#include <vector>
+#include "Error.h"
+using namespace std;
+
+// función que resuelve el problema
+template <class T>
+Set<T> resolver(int k, vector<T> v) {
+	Set<T> sol;
+	int i = 0;
+	while (i < v.size() && k>0) {
+		if (!sol.contains(v[i])) {
+			sol.add(v[i]);
+			k--;
+		}
+		i++;
+	}
+	while(i<v.size()){
+		if (sol.getMin() < v[i] && !sol.contains(v[i])) {
+			sol.removeMin();
+			sol.add(v[i]);
+		}
+		i++;
+	}
+	return sol;
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	char dato;
+
+	cin >> dato;
+	if (!std::cin)
+		return false;
+	
+	int k;
+	cin >> k;
+
+	if (dato == 'N') {
+		vector<int> h;
+		int x;
+		cin >> x;
+		while (x != -1) {
+			h.push_back(x);
+			cin >> x;
+		}
+		Set<int> sol = resolver(k, h);
+		cout << sol;
+	}
+	else {
+		vector<string> h;
+		string x;
+		while (x != "FIN") {
+			h.push_back(x);
+			cin >> x;
+		}
+		Set<string> sol = resolver(k, h);
+		cout << sol;
+	}
+
+	cout << "\n";
+
+	// escribir sol
+
+
+	return true;
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+	while (resuelveCaso())
+		;
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
